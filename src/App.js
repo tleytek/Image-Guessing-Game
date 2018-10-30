4a3e271e7e78615c39abf0b9d75260da43afbba4
tleytek <lucidmat94@gmail.com>
2018-10-30 14:06:03 -0400

    Randomize my images.json array on componentMount

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import React, { Component } from "react";
 import "./App.css";
 import images from "./images.json";
+// import Wrapper from
+import ImageDisplay from "./components/ImageDisplay";
 
 class App extends Component {
+	// Setting this.state.images to the images json array
 	state = {
 		images
 	};
 
+	//Cool, this executes the randomize array on page refresh AKA componentDidMount()
+	componentDidMount() {
+		this.randomizeImages();
+	}
+	randomizeImages = () => {
+		// Randomize this.state.images with images
+		const images = this.state.images.sort((a, b) => {
+			return 0.5 - Math.random();
+		});
+
+		this.setState({ images });
+	};
+
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<img id="images" src={images[0].image} alt="logo" />
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</header>
+			<div>
+				{this.state.images.map(image => (
+					<ImageDisplay
+						name={image.name}
+						randomizeImages={this.randomizeImages}
+					/>
+				))}
 			</div>
 		);
 	}
---
+++ src/components/ImageDisplay/ImageDisplay.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ImageDisplay = props => (
+	<div>
+		<p>{props.name}</p>
+	</div>
+);
+
+export default ImageDisplay;
