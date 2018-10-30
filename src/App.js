6718825a293beff79ee9ae53d9ef624a16c108f5
tleytek <lucidmat94@gmail.com>
2018-10-30 11:43:56 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import images from "./images.json";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		images
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<img id="images" src={images[0].image} alt="logo" />
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
